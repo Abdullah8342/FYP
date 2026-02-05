b44d4439000384f1d598d8ff78ad79b2e8a30485
Abdullah <abdullah.4110r.work@gmail.com>
2026-02-05 14:57:39 +0500

    HelperService Location Admin Done

---
+++ Helper/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.contrib.auth.models import User
+from Service.models import Service
+
+# Create your models here.
+
+
+class Location(models.Model):
+    country = models.CharField(max_length=100)
+    city = models.CharField(max_length=150)
+    area = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return f"{self.country} | {self.city} | {self.area}"
+
+
+class HelperService(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="helper")
+    service = models.ForeignKey(
+        Service, on_delete=models.SET_NULL, null=True, related_name="helper"
+    )
+    location = models.ManyToManyField(Location)
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    experience_year = models.PositiveIntegerField()
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.service.name}"
+
+    class Meta:
+        unique_together = ("user", "service")
